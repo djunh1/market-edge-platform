903cc587c7c526247d33d82e111b10023b14a4c3
djunh1 <djunh1@gmail.com>
2023-08-17 18:12:13 -0400

    add some more boiler plate stuff like css styling, some images, and a few view templates

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('study/', views.study, name="study")
+    path('study/', views.study, name="study"),
+    path('login', views.blank_url, name="login"),
+    path('logout', views.blank_url, name="logout"),
+    path('update_user', views.blank_url, name="update-user")
 ]
 
--- core/views.py
+++ core/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("The Desk")
+    return render(request, 'main.html')
 
 def study(request):
     return HttpResponse("Specific stock study")
+
+def blank_url(request):
+    return HttpResponse("Placeholder")
